7413b50c953167d5eb441def618eb865fb716cd9
Nitish Kumar <nitishhrms@gmail.com>
2020-02-27 20:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app=Flask(__name__)
 @app.route('/')
 def Hello():
     return render_template('index.html')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
 
 
 
