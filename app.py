38d148e8d3b66e122ae435473f4f08afcadd3a62
Nitish Kumar <nitishhrms@gmail.com>
2020-02-27 14:29:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def classification():
     return render_template('index.html',my_target=target1,my_image=image)
 
 if __name__=='__main__':
-    app.jinja_env.cache = {}
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
