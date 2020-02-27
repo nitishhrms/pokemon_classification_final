fa99a38053419c18674b0bd4d8c5f3bead11f9cc
Nitish Kumar <nitishhrms@gmail.com>
2020-02-27 21:49:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def Hello():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html")
+@app.errorhandler(500)
+def internal_error(error):
+
+    return "500 error"
 
 
 
